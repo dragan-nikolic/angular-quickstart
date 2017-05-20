dbf86ff7cadcdedc67184acd63d5d2d0557a56ec
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2017-05-19 20:08:39 -0700

    5. Services - implement sync HeroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core'
-import { Hero } from './hero'
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { Hero } from './hero'
+import { HeroService } from './hero.service'
 
 @Component({
   selector: 'tour-of-heroes',
+
   template: `
     <h1>{{title}}</h1>
 
@@ -78,14 +68,26 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
+
+  providers: [HeroService],
 })
 
 export class AppComponent {
   title = 'Tour of Heroes'
-  heroes = HEROES
+  heroes: Hero[]
   selectedHero: Hero
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
   }
 }
